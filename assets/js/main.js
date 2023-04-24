378ab5e4f721e6a69ab3cf753604c57ef5b5ce96
Nilufar Najiyeva <127186140+nilufarnajiyeva@users.noreply.github.com>
2023-04-25 01:39:17 +0400

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+
+    var a=prompt("ededi daxil edin")
+    var say
+    if(a<50){
+       say=a/3;
+       alert(say);
+        }
+    
+    else if(a>50 && a<100){
+        say=a/5
+        alert(say)
+    }
+    else{
+        say=a/8
+        alert(say)
+    }
+   
+
+   

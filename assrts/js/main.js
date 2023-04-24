f94dd7c5c8fceaf2a811b583d3e141a5e5fe8fac
Nilufar Najiyeva <127186140+nilufarnajiyeva@users.noreply.github.com>
2023-04-25 01:48:21 +0400

    Add files via upload

---
+++ assrts/js/main.js
@@ -0,0 +1,60 @@
+//  function ferq(a,b){
+//     var a,b
+//     return a-b
+// }
+// var f=ferq(9,2)
+// alert (f)
+ 
+
+
+// function cem(a,b){
+//     var a,b
+//     return a+b
+// }
+// var f=cem(9,2)
+// alert (f)
+
+
+// function hasil(a,b){
+//     var a,b
+//     return a*b
+// }
+// var f= hasil(9,2)
+// alert (f)
+
+
+
+// function qismet(a,b){
+//     var a,b
+//     return a/b
+// }
+// var f=qismet(9,3)
+// alert (f)
+
+
+
+// var students = [
+//         {
+//             name:"Məhərrəm",
+//             surname: "Ahmadli",
+//             fathername: "Salamet"
+//         },
+        
+        
+//         {
+//             name: "Nilufer",
+//             surname : "Neciyeva",
+//             fathername:"Elsever"
+//         },
+//         {
+//             name: "Zehra",
+//             surname : "Mehdizade",
+//             fathername:"Mehdi"
+//         }
+//     ];
+//     for(var i = 0; i < students.length;i++){
+//         alert(student[i].surname) ;
+
+       
+//     }
+
